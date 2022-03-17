8c29ff361830e9057d9ad64cfbb7df04f1730e4a
Mathew Potts <55670682+mathewpotts@users.noreply.github.com>
2022-03-17 16:13:38 -0600

    new feature to move downloaded files to database
    Added two command-line options that allow the user to specify paths to authorlist/acknowledgement files and move them to the processing directory.

--- Author/gen_all.py
+++ Author/gen_all.py
@@ -36,14 +36,42 @@ def parse_stats(stats):
     
     return numofauth,numofinstit,numofcount,countinstauth,top5
 
+def if_file_exists(type,path):
+    if path is not None and os.path.isfile(path): # usr has entered in correct path
+        if type == "Author":
+            print(f"Moving {path} -> {os.environ['SRC']}/ta_authorlist.csv\n")
+            os.rename(path, f"{os.environ['SRC']}/ta_authorlist.csv")
+        else:
+            print(f"Moving {path} -> {os.environ['SRC']}/ta_acknowledgements.txt\n")
+            os.rename(path, f"{os.environ['SRC']}/ta_acknowledgements.txt")
+    elif path is None: # If now user path defined check to make sure files exist in $SRC
+        print(f"Using {type} file from {os.environ['SRC']}\n")
+        if type == 'Author' and not os.path.isfile(f"{os.environ['SRC']}/ta_authorlist.csv"):
+            print(f"ERROR: Authorlist file not found. {os.environ['SRC']}/ta_authorlist.csv")
+            sys.exit(1)
+        elif type == 'Acknowledgements' and not os.path.isfile(f"{os.environ['SRC']}/ta_acknowledgements.txt"):
+            print(f"ERROR: Acknowledgement file not found. {os.environ['SRC']}/ta_acknowledgements.txt")
+            sys.exit(1)
+    else: # usr has entered in a incorrect path
+        print(f'ERROR: {type} file not found {auth_path}')
+        sys.exit(1)
+
 def gen_all(new_dates,auth_path=None,ack_path=None):
     # Defining environmental variables
     os.environ['DATE'] = new_dates[0]
     os.environ['SRC']  = '{0}/{1}/src'.format(os.environ['PWD'],new_dates[0])
 
+    # Create Data directory stucture
+    os.system('mkdir -p $SRC;')
+    
+    # Check if files exist at usr defined location and does other file checking before file
+    # generation
+    if_file_exists('Author',auth_path)
+    if_file_exists('Acknowledgements',ack_path)
+    
     # Generate all the author files and store them in a dated file system
     os.system('''
-    mkdir -p $SRC;
+    echo "Generating all TA author list types:";
     echo "LaTex";
     $PWD/ta_author_list.py --csvfile $SRC/ta_authorlist.csv --ackfile $SRC/ta_acknowledgements.txt --output $PWD/$DATE/TA-author-$DATE.tex --format plainLatex;
     echo "Authblk";
@@ -77,21 +105,18 @@ def grab_creds(path):
 
 def main():
     parser = argparse.ArgumentParser(description = "This is a wrapper for Bill Hanlon's TA author list program that will generate all the files need to updated the tadserv author page. (http://tadserv.physics.utah.edu/TA-ICRC-09/index.php/TA_author_list_and_acknowledgements) The files are generated within a dated directory in the same directory as this program, except for the .png file. As of right now you need to save it manually. This program can then post those generated files to the tadserv author page using the python requests library and Beautiful Soup HTML parser.")
-    parser.add_argument('-authdate',metavar='YYYYMMDD', type=str,help='Enter the date of the most recent author list in the following format: YYYYMMDD. (Typically this is the current date)')
-    parser.add_argument('-ackdate',metavar='YYYYMMDD', type=str,help='Enter the date of the most recent acknowledgement list in the following format: YYYYMMDD.')
+    parser.add_argument('-authdate',metavar='YYYYMMDD', type=str,help='Enter the date of the most recent author list in the following format: YYYYMMDD. (Typically this is the current date)',required=True)
+    parser.add_argument('-ackdate',metavar='YYYYMMDD', type=str,help='Enter the date of the most recent acknowledgement list in the following format: YYYYMMDD.',required=True)
     parser.add_argument('-post', default=False, action='store_true', help='POST files to the tadserv wiki.')
+    parser.add_argument('-authpath',metavar='path/to/authfile.csv',type=str,help='Specify a path to the Authorlist.csv file and moves it to the database for processing. (default: uses csv file $PWD/{authdate}/src/ta_authorlist.csv)',default=None)
+    parser.add_argument('-ackpath',metavar='path/to/ackfile.txt',type=str,help='Specify a path to the Acknowledgements.txt file and moves it to the database for processing. (default: uses text file $PWD/{authdate}/src/ta_acknowledgements.txt)',default=None)
     args  = parser.parse_args()
 
     # Create a list of the 'new dates' of the authorlist and acknowledgements
     new_dates = [args.authdate,args.ackdate]
 
-    # Check to make sure the directory exists and if not exit program
-    print('Generating all TA author list types:')
-    if os.path.isdir(new_dates[0]):
-        info  = gen_all(new_dates)
-    else:
-        print("\n{0} doesn't exist. Please create a new dated directory with the appropriate structure. (YYYYMMDD/src)".format(new_dates[0] + '/'))
-        sys.exit(1)
+    # generate files
+    info  = gen_all(new_dates,args.authpath,args.ackpath)
 
     # Get a list of the files generated
     files = [os.path.join(new_dates[0], f) for f in os.listdir(new_dates[0]) if os.path.isfile(os.path.join(new_dates[0],f))]
@@ -112,4 +137,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
